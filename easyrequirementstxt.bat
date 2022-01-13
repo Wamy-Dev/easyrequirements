b714bd62a32dd9781e2748fbcd54155963a5f378
David <25761442+Wamy-Dev@users.noreply.github.com>
2022-01-13 15:06:32 -0600

    Update easyrequirementstxt.bat

--- easyrequirementstxt.bat
+++ easyrequirementstxt.bat
@@ -5,7 +5,7 @@ echo	   / ____/___ ________  __/ __ \___  ____ ___  __(_)_______  ____ ___  ___ 
 echo	  / __/ / __ `/ ___/ / / / /_/ / _ \/ __ `/ / / / / ___/ _ \/ __ `__ \/ _ \/ __ \/ __/ ___// __/ |/_/ __/
 echo	 / /___/ /_/ (__  ) /_/ / _, _/  __/ /_/ / /_/ / / /  /  __/ / / / / /  __/ / / / /_(__  )/ /__>  </ /_  
 echo	/_____/\__,_/____/\__, /_/ |_|\___/\__, /\__,_/_/_/   \___/_/ /_/ /_/\___/_/ /_/\__/____(_)__/_/|_|\__/  
-echo                 /____/              /_/               Made by Wamy
+echo                   /____/              /_/               Made by Wamy
 echo:
 echo This will install any python files needed by the program. Make sure you have pip installed and functioning on your computer. 
 SET /P install= Would you like to install the requirements of this program? Y or N:
