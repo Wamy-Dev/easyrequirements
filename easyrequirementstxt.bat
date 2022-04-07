163e232832ed304ce257975ad331d3cc77e6221b
David <25761442+Wamy-Dev@users.noreply.github.com>
2022-04-07 09:34:45 -0500

    fixed bugs

--- easyrequirementstxt.bat
+++ easyrequirementstxt.bat
@@ -1,11 +1,12 @@
 @echo off
+mode 110,20
 echo:
 echo	    ______                 ____                   _                                __        __       __ 
 echo	   / ____/___ ________  __/ __ \___  ____ ___  __(_)_______  ____ ___  ___  ____  / /______ / /__  __/ /_
-echo	  / __/ / __ `/ ___/ / / / /_/ / _ \/ __ `/ / / / / ___/ _ \/ __ `__ \/ _ \/ __ \/ __/ ___// __/ |/_/ __/
-echo	 / /___/ /_/ (__  ) /_/ / _, _/  __/ /_/ / /_/ / / /  /  __/ / / / / /  __/ / / / /_(__  )/ /__>  </ /_  
-echo	/_____/\__,_/____/\__, /_/ |_|\___/\__, /\__,_/_/_/   \___/_/ /_/ /_/\___/_/ /_/\__/____(_)__/_/|_|\__/  
-echo                   /____/              /_/               Made by Wamy (bat edition)
+echo	  / __/ / __ `/ ___/ / / / /_/ / _ \/ __ `/ / / / / ___/ _ \/ __ `__ \/ _ \/ __ \/ __/ ___// __/ \/_/ __/
+echo	 / /___/ /_/ (__  ) /_/ / _, _/  __/ /_/ / /_/ / / /  /  __/ / / / / /  __/ / / / /_(__  )/ /__)  (/ /_  
+echo	/_____/\__,_/____/\__, /_/ \_\\___/\__, /\__,_/_/_/   \___/_/ /_/ /_/\___/_/ /_/\__/____(_)__/_/\_\\__/  
+echo                    /____/              /_/               Made by Wamy (.bat edition)
 echo:
 echo This will install any python files needed by the program. Make sure you have pip installed and functioning on your computer. 
 SET /P install= Would you like to install the requirements of this program? Y or N:
